6e9dac227ea9c93c7002183f8348e8beca0d241f
wenshijun <412986507@qq.com>
2018-12-07 18:53:31 +0800

    优化swiper

--- js/main.js
+++ js/main.js
@@ -18,14 +18,6 @@ $(function () {
     });
 
 
-    var mySwiper = new Swiper ('.swiper-container', {
-        loop: true,
-        // 如果需要分页器
-        pagination: '.swiper-pagination',
-        autoplay: 5000,
-        autoplayDisableOnInteraction:false
-    })
-
     $("#fp-nav ul li:nth-child(1) a span").append("<i class='iconfont icon-shouye'></i>");
     $("#fp-nav ul li:nth-child(2) a span").append("<i class='iconfont icon-wo'></i>");
     $("#fp-nav ul li:nth-child(3) a span").append("<i class='iconfont icon-rencaitubiao'></i>");
@@ -70,5 +62,54 @@ $(function () {
         $(this).addClass("active").siblings().removeClass("active");
         var index = $(".dot").index(this);
         $('.experience-content').eq(index).addClass("active").siblings().removeClass("active");
+    })
+
+    productionSwiper = new Swiper('#productions .swiper-container', {
+        watchSlidesProgress: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        loop: true,
+        loopedSlides: 5,
+        autoplay: {
+            delay: 10000
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable :true,
+        },
+        on: {
+            progress: function(progress) {
+                for (i = 0; i < this.slides.length; i++) {
+                    var slide = this.slides.eq(i);
+                    var slideProgress = this.slides[i].progress;
+                    modify = 1;
+                    if (Math.abs(slideProgress) > 1) {
+                        modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
+                    }
+                    translate = slideProgress * modify * 260 + 'px';
+                    scale = 1 - Math.abs(slideProgress) / 5;
+                    zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
+                    slide.transform('translateX(' + translate + ') scale(' + scale + ')');
+                    slide.css('zIndex', zIndex);
+                    slide.css('opacity', 1);
+                    if (Math.abs(slideProgress) > 3) {
+                        slide.css('opacity', 0);
+                    }
+                }
+            },
+            setTransition: function(transition) {
+                console.log(transition)
+                for (var i = 0; i < this.slides.length; i++) {
+                    var slide = this.slides.eq(i)
+                    slide.transition(transition);
+                }
+
+            }
+        }
+
     })
 })
